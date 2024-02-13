525f654f0e95e25a3b6e5f62daddb1d177e0a5b1
Catal1nPopa <misterco2002@gmail.com>
2024-02-13 18:35:38 +0200

    Fix bags

--- Lab1/client/Program.cs
+++ Lab1/client/Program.cs
@@ -1,51 +1,52 @@
-﻿using System;
+﻿using System.Net.Sockets;
 using System.Net;
-using System.Net.Sockets;
 using System.Text;
-using System.Threading;
+
+
 
 class Program
 {
     static void Main()
     {
+
         IPAddress serverIp = IPAddress.Parse("127.0.0.1");
         int serverPort = 9000;
 
-        Thread.Sleep(2000);
-
-        while (true)
+        try
         {
-            using (TcpClient client = new TcpClient())
+            using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
-                try
+                client.Connect(new IPEndPoint(serverIp, serverPort));
+                Console.WriteLine("Sunteti conectat la server!");
+
+                while (true)
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Scrieti textul:");
+                    string text = Console.ReadLine();
 
-                    client.Connect(serverIp, serverPort);
-
-                    Console.WriteLine("Introdu textul:\n", Console.ForegroundColor);
-                    string text = Console.ReadLine() ?? "";
-                    Console.WriteLine("\n---------------------\n", Console.ForegroundColor);
-
-                    using (NetworkStream stream = client.GetStream())
+                    if (string.IsNullOrEmpty(text))
                     {
-                        byte[] bytedata = Encoding.UTF8.GetBytes(text);
-                        stream.Write(bytedata, 0, bytedata.Length);
-
-                        byte[] receivedBuffer = new byte[1024];
-                        int bytesRead = stream.Read(receivedBuffer, 0, receivedBuffer.Length);
-                        string receivedText = Encoding.UTF8.GetString(receivedBuffer, 0, bytesRead);
-
-                        Console.WriteLine($"Server: {receivedText}\n");
+                        text = "Clientul a trimis un mesaj fara text";
                     }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Eroare: {ex.Message}");
+
+                    byte[] byteData = Encoding.UTF8.GetBytes(text);
+                    client.Send(byteData);
+
+                    string receivedText = "";
+                    do
+                    {
+                        byte[] receivedBuffer = new byte[1024];
+                        int bytesRead = client.Receive(receivedBuffer);
+                        receivedText += Encoding.UTF8.GetString(receivedBuffer, 0, bytesRead);
+                    } while (client.Available > 0);
+
+                    Console.WriteLine($"Server: {receivedText}\n");
                 }
             }
-
-            // Thread.Sleep(2000);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Eroare: {ex.Message}");
         }
     }
 }
--- Lab1/server/Program.cs
+++ Lab1/server/Program.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
+﻿using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
-using System.Threading;
+using System.Text;
 
 class Server
 {
     public TcpListener serverSocket;
     public List<TcpClient> clients = new List<TcpClient>();
-
+    public static int num;
     public Server()
     {
         serverSocket = new TcpListener(IPAddress.Any, 9000);
@@ -23,82 +20,108 @@ class Server
         acceptClientsThread.Start();
     }
 
-    private void AcceptClients()
+    public void AcceptClients()
     {
         while (true)
         {
             TcpClient client = serverSocket.AcceptTcpClient();
             clients.Add(client);
 
+            Console.WriteLine("\n#########################################################################################\n");
             Console.WriteLine($"Client conectat: {((IPEndPoint)client.Client.RemoteEndPoint).Address}:{((IPEndPoint)client.Client.RemoteEndPoint).Port}");
 
-
             // Pornirea unui fir de execuție pentru a gestiona fiecare client în paralel
-            Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
-            clientThread.Start(client);
+            Thread clientThread = new Thread(() => DataClient(client));
+            clientThread.Start();
         }
     }
 
-    private void HandleClient(object clientObj)
+    public static void DataClient(TcpClient client)
     {
-        TcpClient client = (TcpClient)clientObj;
-        NetworkStream clientStream = client.GetStream();
-
         string welcomeMessage = "Te-ai conectat la server";
         byte[] welcomeMessageBytes = System.Text.Encoding.ASCII.GetBytes(welcomeMessage);
-        clientStream.Write(welcomeMessageBytes, 0, welcomeMessageBytes.Length);
-
-        byte[] buffer = new byte[1024];
-        int bytesRead;
-
-        while (true)
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        using (var stream = client.GetStream())
         {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
             try
             {
-                bytesRead = clientStream.Read(buffer, 0, buffer.Length);
-
-                if (bytesRead == 0)
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) >= 0)
                 {
-                    Console.WriteLine("Clientul a transmis toate datele. Client Deconectat \n ---------------------\n");
-                    break;
-                }
+                    string receivedData = System.Text.Encoding.ASCII.GetString(buffer, 0, bytesRead);
 
-                string receivedData = System.Text.Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                Console.WriteLine($"Mesaj de la client: {receivedData}");
+                    var clientend = (IPEndPoint)client.Client.RemoteEndPoint;
+                    if (string.IsNullOrEmpty(receivedData))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Mesaj de la client : {clientend.Address} :{clientend.Port} \n {receivedData} ", Console.ForegroundColor);
+
+                    }
+                    byte[] responseBytes = Encoding.ASCII.GetBytes($"Serveru a primit mesajul : {clientend.Port}");
+                    stream.Write(responseBytes, 0, responseBytes.Length);
+                    stream.Flush();
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Eroare: {ex.Message}");
-                break; 
             }
         }
-
-        client.Close();
-        clients.Remove(client);
     }
 
+    public static void Start_Program()
+    {
+        if (num != 0 && num > 0) 
+        {
+            Server server = new Server();
+            for (int i = 0; i < num; i++)
+            {
+                string DFPath = "D:\\UTM\\ANUL 3\\SEM 2\\PR\\Programarea-in-Retea\\Lab1\\client\\bin\\Debug\\net6.0\\Client.exe";
+                ProcessStartInfo info = new ProcessStartInfo(DFPath);
+                info.CreateNoWindow = false;
+                info.UseShellExecute = true;
+                Process processChild = Process.Start(info);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Date introduse gresit");
+            
+            MainMenu();
+        }
+    }
 
+    public static bool MainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("1) Alege numarul de clienti");
+        Console.WriteLine("2) Porneste serverul");
+        Console.Write("\rAlege o optiune: ");
 
-
+        switch (Console.ReadLine())
+        {
+            case "1":
+                Console.WriteLine("Introdu numarul de clienti: ");
+                num = int.Parse(Console.ReadLine());
+                MainMenu();
+                return true;
+            case "2":
+                Start_Program();
+                return true;
+            case "3":
+                return false;
+            default:
+                return true;
+        }
+    }
 
     public static void Main()
     {
         Console.ForegroundColor = ConsoleColor.Red;
-
-        int num;
-        Console.WriteLine("Introdu numarul de clienti: ", Console.ForegroundColor);
-        num = int.Parse(Console.ReadLine());
-
-        Server server = new Server();
-        for(int i = 0; i < num; i++)
-        {
-            string DFPath = "D:\\UTM\\ANUL 3\\SEM 2\\PR\\Lab1\\client\\bin\\Debug\\net6.0\\Client.exe";
-            //System.Environment.CurrentDirectory = "D:\\UTM\\ANUL 3\\SEM 2\\PR\\Lab1\\client\\bin\\Debug\\net6.0\\Client.exe";
-            //Process.Start(@DFPath);
-            ProcessStartInfo info = new ProcessStartInfo(DFPath);
-            info.CreateNoWindow = false;
-            info.UseShellExecute = true;
-            Process processChild = Process.Start(info);
-        }
+        MainMenu();
     }
 }
