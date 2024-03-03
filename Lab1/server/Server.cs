f16a60a416428cc2f4d5d81d64d0b99833c393ac
Catal1nPopa <misterco2002@gmail.com>
2024-03-03 19:44:25 +0200

    Lab2

---
+++ Lab1/server/Server.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ServerS
+    {
+        private Socket socket;
+        private List<Socket> connClients;
+
+        public ServerS()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            connClients = new List<Socket>();
+            Start();
+        }
+
+        public void Start()
+        {
+            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000));
+            socket.Listen(10);
+
+            Thread listenerThread = new Thread(Listener);
+            listenerThread.Start();
+        }
+
+        public void Listener()
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Asteptare conexiune...");
+
+                    Socket clientSocket = socket.Accept();
+                    connClients.Add(clientSocket);
+                    
+                    var clientEndPoint = (IPEndPoint)clientSocket.RemoteEndPoint;
+                    string clientInfo = $"Client conectat: IP: {clientEndPoint.Address}, Port: {clientEndPoint.Port}";
+                    Console.WriteLine(clientInfo); 
+
+                    Thread clientThread = new Thread(() => ClientData(clientSocket));
+                    clientThread.Start();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                socket.Close();
+            }
+        }
+
+        public void ClientData(Socket socketClient)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while((bytesRead = socketClient.Receive(buffer)) > 0)
+                {
+                    string dataReceived = Encoding.ASCII.GetString(buffer, 0 , bytesRead);
+                    if (string.IsNullOrEmpty(dataReceived))
+                    {
+                        Console.WriteLine("Mesaj gol!");
+                        continue;
+                    }
+                    broadcast($"Client {socketClient.RemoteEndPoint}: {dataReceived}", socketClient);
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+
+        public void broadcast(string message, Socket socketSender)
+        {
+                foreach(var clientSocket in connClients)
+                {
+                    if (clientSocket != socketSender)
+                    {
+                        try
+                        {
+                            byte[] buffer = Encoding.ASCII.GetBytes(message);
+                            clientSocket.Send(buffer);
+                        }
+                        catch (Exception ex) 
+                        {
+                            Console.WriteLine($"Eroare la trimitere broadcast catre: {clientSocket.RemoteEndPoint}: {ex.Message}");
+                        }
+                    }
+                }
+            
+        }
+    }
+}
---
+++ Lab1/server/StartProgram.cs
@@ -0,0 +1,79 @@
+﻿
+using Server;
+using System;
+using System.Diagnostics;
+
+class StartProgram
+{
+    public static int num = 0;
+    static void Main(string[] arg)
+    {
+        MainMenu();
+    }
+    public static bool MainMenu()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Clear();
+        Console.WriteLine("1) Alege numarul de clienti");
+        Console.WriteLine("2) Porneste serverul");
+        Console.Write("\rAlege o optiune: ");
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                Console.WriteLine("Introdu numarul de clienti: ");
+                try
+                {
+                    num = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+                MainMenu();
+                return true;
+            case "2":
+                if(num != 0 && num > 0)
+                    {
+                     Start_Program();
+                    }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Nu ai introdus numarul de clienti. \n 1 - afiseaza meniu \n 0 - exit");
+                    int s = int.Parse(Console.ReadLine());
+                    if( s == 1)
+                    {
+                        MainMenu();
+                    }
+                    else
+                    {
+                        //break;
+                        return false;
+                    }
+                }
+                return true;
+            default:
+                MainMenu();
+                return true;
+        }
+    }
+    public static void Start_Program()
+    {
+        if (num != 0 && num > 0)
+        {
+            ServerS server = new ServerS();
+            for (int i = 0; i < num; i++)
+            {
+                string DFPath = "D:\\UTM\\ANUL 3\\SEM 2\\PR\\Programarea-in-Retea\\Lab1\\client\\bin\\Debug\\net6.0\\Client.exe";
+                ProcessStartInfo info = new ProcessStartInfo(DFPath);
+                info.CreateNoWindow = false;
+                info.UseShellExecute = true;
+                Process processChild = Process.Start(info);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Date introduse gresit");
+
+            MainMenu();
+        }
+    }
+}
