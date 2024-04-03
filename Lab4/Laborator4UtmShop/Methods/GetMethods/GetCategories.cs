406bf1487f3c9e712dc2b3ad0b67daa64ab9863d
Catal1nPopa <misterco2002@gmail.com>
2024-04-04 00:29:20 +0300

    Created all methods for lab4

---
+++ Lab4/Laborator4UtmShop/Methods/GetMethods/GetCategories.cs
@@ -0,0 +1,62 @@
+﻿using Laborator4UtmShop.Entities;
+using Newtonsoft.Json;
+
+namespace Laborator4UtmShop.Methods.GetMethods
+{
+    public class GetCategories
+    {
+        private static async Task<List<Category>> GetCategoriesFromApi()
+        {
+            string apiUrl = "https://localhost:44370/api/Category/categories";
+            List<Category> categories = new List<Category>();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                        var response = await client.GetAsync(apiUrl);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string responseBody = await response.Content.ReadAsStringAsync();
+                            dynamic data = JsonConvert.DeserializeObject(responseBody);
+
+                            foreach (var item in data)
+                            {
+                                long id = (long)item["id"];
+                                string name = (string)item["name"];
+                                int itemsCount = (int)item["itemsCount"];
+                                var category = new Category
+                                {
+                                    Id = id,
+                                    Title = name,
+                                    Products = itemsCount
+                                };
+                                categories.Add(category);
+                            }
+                        }
+                        return categories;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return new List<Category>();
+            }
+        }
+
+        public static async Task getCategory()
+        {
+            try
+            {
+                List<Category> categories = await GetCategoriesFromApi();
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"Category ID: {category.Id}, Name: {category.Title}, Products Count: {category.Products}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while getting categories: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Lab4/Laborator4UtmShop/Methods/PutMethods/ChangeTittle.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator4UtmShop.Methods.PutMethods
+{
+    public class ChangeTittle
+    {
+        private static async Task<string> ChangeTitleApi(int idCategory, string newTitle)
+        {
+            string apiUrl = $"https://localhost:44370/api/Category/{idCategory}";
+
+            var data = new
+            {
+                title = newTitle
+            };
+            var jsonData = JsonConvert.SerializeObject(data);
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                    var response = await client.PutAsync(apiUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseContent = await response.Content.ReadAsStringAsync();
+                        return responseContent;
+                    }
+                    return $"error: {response.StatusCode}";
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error request {ex.Message}");
+                throw;
+            }
+        }
+
+        public static async Task ChangeTitleCategoryWithId()
+        {
+            Console.WriteLine("Give category ID:\n");
+            int categoryId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give new title:\n");
+            string newTitle = Console.ReadLine();
+
+            string response = await ChangeTitleApi(categoryId, newTitle);
+            Console.WriteLine(response);
+        }
+    }
+}
